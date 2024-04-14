d5ed0369ad628db74f6c8070d0d409c69b91432a
AtsuAtsu <58623243+AtsuAtsu0120@users.noreply.github.com>
2024-04-14 21:05:56 +0900

    feat: 正しくInject出来てなかった問題を修正

--- Assets/SuperOthello/Model/OthelloLogic.cs
+++ Assets/SuperOthello/Model/OthelloLogic.cs
@@ -12,17 +12,16 @@ namespace SuperOthello.Model
         private readonly ISubscriber<CellPosition> _putSubscriber;
         private OthelloGame _game;
         
-        
-        public void Initialize()
-        {
-            
-        }
-        
         [Inject] 
         public OthelloLogic(ISubscriber<CellPosition> putSubscriber, IPublisher<CellState[,]> boardPublisher, IPublisher<IEnumerable<(int row, int column)>> canPutPublisher)
         {
+            _putSubscriber = putSubscriber;
             _game = new(boardPublisher, canPutPublisher);
             _putSubscriber.Subscribe(_ => Debug.Log("aaa"));
+        }
+        public void Initialize()
+        {
+            
         }
     }
 }
